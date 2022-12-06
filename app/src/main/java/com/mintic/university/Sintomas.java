72f5f94200583c8ddb3bf0daa9612eca3ef0af01
= <=>
2022-12-06 09:54:41 -0500

    repositorio inicial

---
+++ app/src/main/java/com/mintic/university/Sintomas.java
@@ -0,0 +1,13 @@
+package com.mintic.university;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sintomas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sintomas);
+    }
+}
